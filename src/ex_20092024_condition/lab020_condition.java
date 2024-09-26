e639eaa09694b1cf278547c4daa86b2ab2cce1e7
samgita <sangitadhali1994@gmail.com>
2024-09-26 20:03:10 +0530

    1st commit

---
+++ src/ex_20092024_condition/lab020_condition.java
@@ -0,0 +1,15 @@
+package ex_20092024_condition;
+
+import java.sql.SQLOutput;
+
+public class lab020_condition {
+    public static void main(String[] args) {
+
+        int tushar = 4;
+        int prabhu = 7;
+        String result = tushar > prabhu ? "Tushar won" : "Prabhu won";
+        System.out.println(result);
+
+
+    }
+}
