822e351657a0167ecc32d78938c5bb0181395762
samgita <sangitadhali1994@gmail.com>
2024-09-26 10:57:21 +0530

    2nd commit

---
+++ src/Sept/ex_04092024/Lab004.java
@@ -0,0 +1,8 @@
+package Sept.ex_04092024;
+
+public class Lab004 {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- src/ex_15092024_DataType/lab019.java
+++ src/ex_15092024_DataType/lab019.java
@@ -2,6 +2,6 @@ package ex_15092024_DataType;
 
 public class lab019 {
     public static void main(String[] args) {
-        // System.out.println("HelloWorld");
+        System.out.println("HelloWorld");
     }
 }
